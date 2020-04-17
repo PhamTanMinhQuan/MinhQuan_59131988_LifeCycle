5c6c2dcd09dab88e040fa19ee64aecf8aa86c2c2
PhamTanMinhQuan <rotpro41@gmail.com>
2020-04-17 12:56:57 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/minhquan_59131988_lifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.minhquan_59131988_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
